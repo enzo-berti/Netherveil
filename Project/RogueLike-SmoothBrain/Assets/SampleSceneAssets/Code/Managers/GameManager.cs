7b2b2a811dea5ca703c1daa66b91c3446d3a108e
EnzoTatou <enzoberti@protonmail.com>
2024-01-10 18:39:28 +0100

    Update GameManager.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/GameManager.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/GameManager.cs
@@ -2,20 +2,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private GameManager instance;
-
-    public GameManager Instance 
-    { 
-        get 
-        { 
+    static private GameManager instance;
+    static public GameManager Instance
+    {
+        get
+        {
             if (instance == null)
             {
-                GameObject obj = new GameObject(GetType().Name);
+                GameObject obj = new GameObject(typeof(GameManager).Name);
                 instance = obj.AddComponent<GameManager>();
                 DontDestroyOnLoad(obj);
             }
 
             return instance;
-        } 
+        }
+    }
+
+    public readonly Seed seed = new Seed();
+
+    private void Instantiate()
+    {
+        seed.Generate();
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instantiate();
     }
 }
