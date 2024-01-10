6c6cb4a1823a85bfbc6da385a5c0258bb7494b87
Toraenor <olivier.maurin.development@gmail.com>
2024-01-10 16:34:17 +0100

    added a 3-combo sword systrem

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Player/PlayerAnimation.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Player/PlayerAnimation.cs
@@ -8,6 +8,7 @@ public class PlayerAnimation : MonoBehaviour
 {
     PlayerController controller;
     public Animator animator;
+    bool hasTriggeredAttack = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +24,13 @@ public class PlayerAnimation : MonoBehaviour
 
     public void Attack(InputAction.CallbackContext ctx)
     {
-        if (controller.hero.State == Hero.PlayerState.MOVE)
+        if (controller.hero.State == Hero.PlayerState.MOVE || controller.hero.State == Hero.PlayerState.ATTACK)
         {
-            animator.SetTrigger("BasicAttack");
             controller.hero.State = Hero.PlayerState.ATTACK;
+            animator.SetTrigger("BasicAttack");
+            hasTriggeredAttack = true;
+            controller.ComboCount = (++controller.ComboCount) % controller.MAX_COMBO_COUNT;
+            animator.SetInteger("ComboCount", controller.ComboCount);
         }
     }
 
@@ -34,14 +38,26 @@ public class PlayerAnimation : MonoBehaviour
     {
         if(controller.hero.State == Hero.PlayerState.MOVE)
         {
-            animator.SetTrigger("Dash");
             controller.hero.State = Hero.PlayerState.DASH;
             controller.dashDir = controller.LastDir;
+            animator.SetTrigger("Dash");
         }
     }
 
     public void EndOfSpecialAnimation() //triggers for dash and attack animations to reset currentState
     {
         controller.hero.State = Hero.PlayerState.MOVE;
+        hasTriggeredAttack = false;
+    }
+
+    public void EndOfSpecialAnimationAttack() //triggers for dash and attack animations to reset currentState
+    {
+        controller.hero.State = Hero.PlayerState.MOVE;
+        if(hasTriggeredAttack)
+        {
+            hasTriggeredAttack = false;
+            controller.ComboCount = -1;
+            animator.SetInteger("ComboCount", controller.ComboCount);
+        }
     }
 }
