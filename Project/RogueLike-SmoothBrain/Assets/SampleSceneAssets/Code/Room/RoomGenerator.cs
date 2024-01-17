0dab45e523788acefe988a09fe67cc7c38f683d5
EnzoTatou <enzoberti@protonmail.com>
2024-01-17 23:47:31 +0100

    MORE COOKING

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Room/RoomGenerator.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Room/RoomGenerator.cs
@@ -5,7 +5,7 @@ public class RoomGenerator : MonoBehaviour
 {
     private void Generate(int roomGenerated = 0)
     {
-        GameManager.Instance.seed.Set(123456);
+        // DEBUG GameManager.Instance.seed.Set(123456);
 
         List<GameObject> rooms = new List<GameObject>();
 
