658b405d3b8d4d42fe17d05c5dd842900d2096f7
Léo <leo413568@gmail.com>
2024-01-29 10:54:11 +0100

    Add name and description to other item already create

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/BootOfSwiftness.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/BootOfSwiftness.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class BootOfSwiftness : Item, IPassiveItem
 {
+    private float speedStat = 1.5f;
+
     public void OnRetrieved()
     {
         Hero player = GameObject.FindGameObjectWithTag("Player").GetComponent<Hero>();
-        player.Stats.IncreaseValue(Stat.SPEED, 1.5f);
+        player.Stats.IncreaseValue(Stat.SPEED, speedStat);
+        RarityTier = Rarity.RARE;
+        Name = "<color=\"blue\">Boots of Swiftness";
+        Description = "Heightens player speed, granting swift agility to outpace challenges in the blink of an eye.\n" +
+            "<color=\"green\">Speed: +" + speedStat.ToString();
     }
 }
--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/MonsterHeart.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/MonsterHeart.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MonsterHeart : Item, IPassiveItem
 {
     private int maxLifeStat = 10;
-    private int corruptionStat = 10;
+    private int corruptionStat = 2;
 
     public void OnRetrieved()
     {
@@ -11,7 +11,7 @@ public class MonsterHeart : Item, IPassiveItem
         player.Stats.SetValue(Stat.MAX_HP, maxLifeStat);
         player.Stats.SetValue(Stat.CORRUPTION, corruptionStat);
         RarityTier = Rarity.RARE;
-        Name = "Monster's heart";
+        Name = "<color=\"blue\">Monster's heart";
         Description = "Boosts max health while corrupting the player, a perilous choice between vitality and darkness.\n" +
             "<color=\"green\">Max health: +" + maxLifeStat.ToString() + "  <color=\"purple\">Corruption: +" + corruptionStat.ToString();
     }
