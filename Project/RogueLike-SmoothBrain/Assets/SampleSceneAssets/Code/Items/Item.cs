151a2beef8c2fee9bed51a8690be74c6ec10deb7
naoki18 <jadras.dunkan@gmail.com>
2024-01-29 15:33:31 +0100

    ça marche enfin

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/Item.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/Item.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 
 public class Item : MonoBehaviour, IInterractable
 {
-    ItemData data;
+    [SerializeField]ItemData data;
+    private void Update()
+    {
+        if( Vector2.Distance( GameObject.FindWithTag("Player").transform.position, transform.position ) < 10 )
+        {
+            Interract();
+        }
+    }
     public void Interract()
     {
-        GameObject.FindWithTag("Player").GetComponent<Inventory>().AddItem(Activator.CreateInstance(data.effect.GetClass()) as ItemEffect);
+        GameObject.FindWithTag("Player").GetComponent<Hero>().Inventory.AddItem(Activator.CreateInstance(data.effect.GetClass()) as ItemEffect);
+        Destroy(this.gameObject);
     }
 }
