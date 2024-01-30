039b8ce873a2bd6ac939034979cf7f0579cee2b3
naoki18 <jadras.dunkan@gmail.com>
2024-01-30 15:30:50 +0100

    remove PlayerAttack

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Player/PlayerAttack.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Player/PlayerAttack.cs
@@ -8,22 +8,5 @@ public class PlayerAttack : MonoBehaviour
     {
         Debug.DrawLine(this.transform.position, this.transform.position + this.transform.forward * this.gameObject.GetComponent<Hero>().Stats.GetValueStat(Stat.ATK_RANGE));
     }
-    public void ThrowSpear()
-    {
-        if(!GetComponent<PlayerInput>().LaunchedAttack)
-        {
-            Spear spear = weapon.GetComponent<Spear>();
-
-            // If spear is being thrown we can't recall this attack
-            if (spear.IsThrowing) return;
-            if (!spear.IsThrown)
-            {
-                spear.Throw(this.transform.position + this.transform.forward * this.gameObject.GetComponent<Hero>().Stats.GetValueStat(Stat.ATK_RANGE));
-            }
-            else
-            {
-                spear.Return();
-            }
-        }
-    }
+    
 }
