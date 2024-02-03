bfd6a5d782510dce4f5c9a0f9b7dcbd3b927b455
EnzoTatou <enzoberti@protonmail.com>
2024-02-03 13:52:47 +0100

    Update dictionnary extension

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Extensions/DictionaryExtensions.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Extensions/DictionaryExtensions.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
 
 static public class DictionaryExtensions
 {
     /// <summary>
-    /// Count the number of values in every countainer of the dictionnary
+    /// Count the number of values in every list of the dictionnary
     /// </summary>
     /// <returns>Number of values in dictionnary</returns>
     static public int CountValues<TKey, TValue>(this Dictionary<TKey, List<TValue>> dictionnary)
@@ -13,6 +13,21 @@ static public class DictionaryExtensions
         foreach (var keyValuePair in dictionnary)
         {
             count += keyValuePair.Value.Count;
+        }
+
+        return count;
+    }
+
+    /// <summary>
+    /// Count the number of values in every countainer of the dictionnary
+    /// </summary>
+    /// <returns>Number of values in dictionnary</returns>
+    static public int CountValues<TKey, TValue>(this Dictionary<TKey, IEnumerable<TValue>> dictionnary)
+    {
+        int count = 0;
+        foreach (var keyValuePair in dictionnary)
+        {
+            count += keyValuePair.Value.Count();
         }
 
         return count;
