5f6908ce0b5a7374bcb5fdea2dddc391c69a9e5f
naoki18 <jadras.dunkan@gmail.com>
2024-02-26 17:00:19 +0100

    Some editor change on stats
    HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/GeneralEntities/StatInfo.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/GeneralEntities/StatInfo.cs
@@ -1,4 +1,8 @@
 using System;
+using UnityEditor.UIElements;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEngine;
 
 // Si vous ajoutez un truc, ajoutez le à la fin
 public enum Stat
@@ -26,4 +30,94 @@ public class StatInfo
 {
     public Stat stat;
     public float value;
+    public bool hasMaxStat;
+    public Stat maxStat;
 }
+
+
+#if UNITY_EDITOR
+///// Affichage carré dans l'éditeur /////
+
+[CustomPropertyDrawer(typeof(StatInfo))]
+public class StatInfoDrawerUIE : PropertyDrawer
+{
+    SerializedProperty statProperty;
+    SerializedProperty valueProperty;
+    SerializedProperty hasMaxStatProperty;
+    SerializedProperty maxStatProperty;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+        statProperty = property.FindPropertyRelative("stat");
+        valueProperty = property.FindPropertyRelative("value");
+        hasMaxStatProperty = property.FindPropertyRelative("hasMaxStat");
+        maxStatProperty = property.FindPropertyRelative("maxStat");
+        Rect foldoutBox = new Rect(position.min.x, position.min.y, position.size.x, EditorGUIUtility.singleLineHeight);
+        property.isExpanded =  EditorGUI.Foldout(foldoutBox, property.isExpanded, label);
+        if(property.isExpanded)
+        {
+            DrawStat(position);
+            DrawValue(position);
+            DrawBoolMaxStat(position);
+            if(hasMaxStatProperty.boolValue)
+            {
+                DrawMaxStat(position);
+            }
+        }
+        EditorGUI.EndProperty();
+    }
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        int totalLine = 1;
+        if(property.isExpanded)
+        {
+            totalLine += 4;
+        }
+        return EditorGUIUtility.singleLineHeight * totalLine;
+    }
+    private void DrawStat(Rect position)
+    {
+        float posX = position.min.x;
+        float posY = position.min.y + EditorGUIUtility.singleLineHeight;
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(posX, posY, width, height);
+        EditorGUI.PropertyField(drawArea, statProperty);
+    }
+
+    private void DrawValue(Rect position)
+    {
+        float posX = position.min.x;
+        float posY = position.min.y + EditorGUIUtility.singleLineHeight * 2;
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(posX, posY, width, height);
+        EditorGUI.PropertyField(drawArea, valueProperty);
+    }
+
+    private void DrawBoolMaxStat(Rect position)
+    {
+        float posX = position.min.x;
+        float posY = position.min.y + EditorGUIUtility.singleLineHeight * 3;
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(posX, posY, width, height);
+        EditorGUI.PropertyField(drawArea, hasMaxStatProperty);
+    }
+
+    private void DrawMaxStat(Rect position)
+    {
+        float posX = position.min.x;
+        float posY = position.min.y + EditorGUIUtility.singleLineHeight * 4;
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(posX, posY, width, height);
+        EditorGUI.PropertyField(drawArea, maxStatProperty);
+    }
+}
+#endif
