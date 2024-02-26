bcd1a47723d3eba6589aea2f76722a3b4cd3dbba
Dorian <thesothief@gmail.com>
2024-02-26 12:38:49 +0100

    Rework cinematics
    Exit cinematics
    Start game

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/Menu/MainMenu.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/Menu/MainMenu.cs
@@ -1,40 +1,40 @@
 using Cinemachine;
+using System;
 using System.Collections;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private MeshButton[] meshButtons;
+    [SerializeField] private TMP_Text[] floatingTexts;
+
     public void PlayGame()
     {
         if (Camera.main.GetComponent<CinemachineBrain>().IsBlending) return;
-        StartCoroutine(StartEnumerator());
+        StartCoroutine(EventEnumerator(LoadGame));
     }
 
-    public void ExitGame()
-    {
-        if (Camera.main.GetComponent<CinemachineBrain>().IsBlending) return;
-        StartCoroutine(ExitEnumerator());
-    }
-
-    public IEnumerator StartEnumerator()
+    public IEnumerator EventEnumerator(Action eventMethod)
     {
         yield return new WaitForEndOfFrame();
         CinemachineBrain brain = Camera.main.GetComponent<CinemachineBrain>();
         yield return new WaitWhile(() => brain.IsBlending);
 
+        eventMethod?.Invoke();
+    }
+
+    public void LoadGame()
+    {
         // TODO : Scene loader
         //SceneManager.LoadSceneAsync();
         Debug.Log("Game started");
     }
 
-    public IEnumerator ExitEnumerator()
+    public void Quit()
     {
-        yield return new WaitForEndOfFrame();
-        CinemachineBrain brain = Camera.main.GetComponent<CinemachineBrain>();
-        yield return new WaitWhile(() => brain.IsBlending);
-
 #if UNITY_EDITOR
         if (EditorApplication.isPlaying)
         {
