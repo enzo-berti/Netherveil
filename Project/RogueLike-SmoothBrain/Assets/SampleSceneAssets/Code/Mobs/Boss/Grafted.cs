8e33a33e5066c0455d6b744fc57d18c37a82d0c9
Toraenor <olivier.maurin.development@gmail.com>
2024-03-04 15:27:39 +0100

    added damage numbers

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Mobs/Boss/Grafted.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Mobs/Boss/Grafted.cs
@@ -97,6 +97,7 @@ public class Grafted : Mobs, IAttacker, IDamageable, IMovable, IBlastable
     public void ApplyDamage(int _value)
     {
         Stats.IncreaseValue(Stat.HP, -_value, false);
+        DamageManager.Instance.CreateDamageText(_value, transform.position + Vector3.up * 2, false, 1);
         if (stats.GetValue(Stat.HP) <= 0)
         {
             Death();
