5c289f1895a4689e5c51d2ca08ff80003507bdf8
naoki18 <jadras.dunkan@gmail.com>
2024-03-11 14:57:27 +0100

    status finito pour le moment

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Tank.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Tank.cs
@@ -25,7 +25,7 @@ public class Tank : Mobs, IAttacker, IDamageable, IMovable, IBlastable
         Destroy(gameObject);
     }
 
-    public void ApplyDamage(int _value)
+    public void ApplyDamage(int _value, bool hasAnimation = true)
     {
         Stats.IncreaseValue(Stat.HP, -_value, false);
         FloatingTextGenerator.CreateDamageText(_value, transform.position + Vector3.up * 2, false, 1);
