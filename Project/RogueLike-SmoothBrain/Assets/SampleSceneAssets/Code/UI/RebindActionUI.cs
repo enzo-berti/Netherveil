ca4fad4cf9638db60b3573b38a9e998614b78633
Toraenor <olivier.maurin.development@gmail.com>
2024-03-15 09:49:23 +0100

    amazing rebind fix
    this fix, indeed, is a work of art, let me explain...The problem was displayed as this : the basic attack and the charged attack are binded to the same button, so they will be counted as duplicate bindings for the rebinding script. CRAP! HOW CAN I FIX THIS?? Then, i got an idea, a genious idea, an idea that not a lot of of people could have had, and i figured out that there is a list of interactions stored inside the binding that represents if the binding needs to be hold, just tapped, multi-tapped etc... so if i had a check on the duplicate binding to check not only the binding but also the interactions of the binding, i would get the perfect check.
    
    This is my story,
    Olivier Maurin

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/RebindActionUI.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/RebindActionUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 ////TODO: localization support
 
@@ -217,6 +218,13 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             if (!ResolveActionAndBinding(out var action, out var bindingIndex))
                 return;
 
+            // Check for duplicate bindings before resetting to default, and if found, swap the two controls.
+            if (SwapResetBindings(action, bindingIndex))
+            {
+                UpdateBindingDisplay();
+                return;
+            }
+
             if (action.bindings[bindingIndex].isComposite)
             {
                 // It's a composite. Remove overrides from part bindings.
@@ -228,6 +236,36 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 action.RemoveBindingOverride(bindingIndex);
             }
             UpdateBindingDisplay();
+        }
+
+        /// <summary>
+        /// Check for duplicate rebindings when the binding is going to be set to default.
+        /// </summary>
+        /// <param name="action">InputAction we are resetting.</param>
+        /// <param name="bindingIndex">Current index of the control we are rebinding.</param>
+        /// <returns></returns>
+        private bool SwapResetBindings(InputAction action, int bindingIndex)
+        {
+            // Cache a reference to the current binding.
+            InputBinding newBinding = action.bindings[bindingIndex];
+            // Check all of the bindings in the current action map to make sure there are no duplicates.
+            for (int i = 0; i < action.actionMap.bindings.Count; ++i)
+            {
+                InputBinding binding = action.actionMap.bindings[i];
+                if (binding.action == newBinding.action)
+                {
+                    continue;
+                }
+                if (binding.effectivePath == newBinding.path && binding.interactions == newBinding.interactions)
+                {
+                    Debug.Log("Duplicate binding found for reset to default: " + newBinding.effectivePath);
+                    // Swap the two actions.
+                    action.actionMap.FindAction(binding.action).ApplyBindingOverride(i, newBinding.overridePath);
+                    action.RemoveBindingOverride(bindingIndex);
+                    return true;
+                }
+            }
+            return false;
         }
 
         /// <summary>
@@ -341,7 +379,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 {
                     continue;
                 }
-                if(binding.effectivePath == newBinding.effectivePath)
+                if(binding.effectivePath == newBinding.effectivePath && binding.interactions == newBinding.interactions)
                 {
                     Debug.Log("Duplicate Binding Found : " + newBinding.effectivePath);
                     return true;
@@ -353,7 +391,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             {
                 for(int i = 1; i < bindingIndex; i++) 
                 {
-                    if (action.bindings[i].effectivePath == newBinding.effectivePath)
+                    if (action.bindings[i].effectivePath == newBinding.effectivePath && action.bindings[i].interactions == newBinding.interactions)
                     {
                         Debug.Log("Duplicate Binding Found : " + newBinding.effectivePath);
                         return true;
