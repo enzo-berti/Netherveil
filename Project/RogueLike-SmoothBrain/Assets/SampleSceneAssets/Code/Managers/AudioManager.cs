e62a4ab947f0004095201877e53519df8a55777a
Toraenor <olivier.maurin.development@gmail.com>
2024-03-25 10:27:35 +0100

    Update AudioManager.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/AudioManager.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/AudioManager.cs
@@ -5,7 +5,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using static UnityEngine.Timeline.AnimationPlayableAsset;
 
 public class CustomEventTrigger : EventTrigger
 {
