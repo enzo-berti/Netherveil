40e9e171805608c4c2363d868a7b59695c540579
naoki18 <jadras.dunkan@gmail.com>
2024-03-28 16:21:29 +0100

    Range pas fini

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Range.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Range.cs
@@ -21,24 +21,27 @@ public class Range : Mobs, IRange
     public IAttacker.HitDelegate OnHit { get => onHit; set => onHit = value; }
     public IAttacker.AttackDelegate OnAttack { get => onAttack; set => onAttack = value; }
 
-    private float fleeTimer = 4;
     [SerializeField] GameObject pfBomb;
     [Header("Range Parameters")]
     [SerializeField, Min(0)] private float staggerDuration;
 
-    private bool isFighting = false;
-    //private RangeState state;
-
-    private bool isFleeing;
-    private Vector3 fleeTarget;
 
     private float staggerImmunity;
     private float staggerTimer;
 
-    private float FleeingRange => (int)stats.GetValue(Stat.ATK_RANGE) / 2f;
+    private bool canAttack = true;
+    private bool isFleeing = false;
+    private bool isGoingOnPlayer = false;
+    private bool isAttacking = false;
+    private bool atPosToAttack = false;
+    private float DistanceToFlee
+    {
+        get => stats.GetValue(Stat.ATK_RANGE) / 2f;
+    }
 
     Animator animator;
 
+    Transform playerTransform;
     public List<Status> StatusToApply { get => statusToApply; }
     protected override void Start()
     {
@@ -46,8 +49,13 @@ public class Range : Mobs, IRange
 
         // getter(s) reference
         animator = GetComponentInChildren<Animator>();
+        playerTransform = GameObject.FindWithTag("Player").transform;
     }
 
+    private bool IsTurnToPlayer()
+    {
+        return false;
+    }
     protected override IEnumerator EntityDetection()
     {
         while (true)
@@ -57,12 +65,6 @@ public class Range : Mobs, IRange
                 yield return null;
                 continue;
             }
-
-            nearbyEntities = PhysicsExtensions.OverlapVisionCone(transform.position, isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS), (int)stats.GetValue(Stat.VISION_RANGE), transform.forward, LayerMask.GetMask("Entity"))
-                    .Select(x => x.GetComponent<Entity>())
-                    .Where(x => x != null && x != this)
-                    .OrderBy(x => Vector3.Distance(x.transform.position, transform.position))
-                    .ToArray();
 
             yield return new WaitForSeconds(0.5f);
         }
@@ -77,67 +79,52 @@ public class Range : Mobs, IRange
             if (!agent.enabled)
                 continue;
 
+            float distanceFromPlayer = Vector3.Distance(transform.position, playerTransform.position);
 
-            Hero player = null;
-            Tank[] tanks = null;
-            if (nearbyEntities.Length > 0)
+            // S'il a atteint sa position et qu'il est toujours à range pour attaquer il va le faire
+            if (!isGoingOnPlayer && !isFleeing && canAttack && distanceFromPlayer <= stats.GetValue(Stat.ATK_RANGE))
             {
-                player = nearbyEntities
-                    .Select(x => x.GetComponent<Hero>())
-                    .Where(x => x != null)
-                    .FirstOrDefault();
-
-                tanks = nearbyEntities
-                    .Select(x => x.GetComponent<Tank>())
-                    .Where(x => x != null)
-                    .OrderBy(x => Vector3.Distance(x.transform.position, transform.position))
-                    .ToArray();
+                // Il se tourne vers le player pour l'attaquer
+                //StartCoroutine(FaceToPlayer(player.transform));
+                this.transform.LookAt(playerTransform.transform.position);
+                // Quand il est tourné vers le player il lui envoie une bombe
+                StartCoroutine(LongRangeAttack(playerTransform.transform));
             }
-
-            // Update timer fuite
-            fleeTimer = fleeTimer > 0 ? fleeTimer -= Time.deltaTime : 0;
-            if (player)
+            // Si l'ennemi peut attaquer il va se deplacer à range du player
+            else if (!isGoingOnPlayer && !isFleeing && canAttack && !isAttacking)
             {
-                isFighting = true;
-                float distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
-
-                // si le joueur est trop près et que la fuite est dispo
-                if (distanceFromPlayer < FleeingRange && fleeTimer == 0f)
-                {
-                    isFleeing = true;
-                    fleeTimer = 4f;
-
-                    Vector3 playerToEnemyVector = transform.position - player.transform.position;
-                    playerToEnemyVector.Normalize();
-
-                    fleeTarget = player.transform.position + playerToEnemyVector * (int)stats.GetValue(Stat.ATK_RANGE);
-
-                    //MoveTo(fleeTarget);
-                }
-                // sinon si dans la zone d'attaque, attaquer
-                else if (distanceFromPlayer < stats.GetValue(Stat.ATK_RANGE))
-                {
-                    LongRangeAttack(player.transform.position);
-                    yield return new WaitForSeconds(2f);
-
-                }
-                // sinon si pas en fuite, avancer vers le joueur
-                else if (!isFleeing)
-                {
-                    Vector3 direction = (player.transform.position - transform.position).normalized;
-                    float factor = (int)stats.GetValue(Stat.VISION_RANGE) - (int)stats.GetValue(Stat.ATK_RANGE);
-                    MoveTo(transform.position + direction * factor);
-                }
+                Debug.Log("Going on player");
+                isGoingOnPlayer = true;
+                atPosToAttack = false;
+                Vector2 playerPosXZ = new Vector2(playerTransform.position.x, playerTransform.position.z);
+                // Minus an offset to avoid the enemy to have to go on the player each time this one move 
+                Vector2 Point2DToReach = GetPointOnCircle(playerPosXZ, stats.GetValue(Stat.ATK_RANGE) - 3f);
+                Vector3 point3DToReach = new Vector3(Point2DToReach.x, playerTransform.position.y, Point2DToReach.y);
+                MoveTo(point3DToReach);
             }
-            else if (!agent.hasPath)
+            // Flee
+            else if (!isGoingOnPlayer && !isFleeing && !isAttacking && distanceFromPlayer <= DistanceToFlee)
             {
-                if (isFleeing)
+                Debug.Log("fleeing");
+                Vector2 playerPosXZ = new Vector2(playerTransform.position.x, playerTransform.position.z);
+                Vector2 Point2DToReach = GetPointOnCircle(playerPosXZ, stats.GetValue(Stat.ATK_RANGE) + 10);
+                Vector3 point3DToReach = new Vector3(Point2DToReach.x, playerTransform.position.y, Point2DToReach.y);
+                Vector3 direction = this.transform.position - point3DToReach;
+                this.transform.forward = direction;
+                MoveTo(point3DToReach);
+                isFleeing = true;
+            }
+            
+            if (!agent.hasPath)
+            {
+                if(isFleeing)
                 {
-                    isFleeing = false;
+                    isFleeing = false; 
+                    this.transform.LookAt(playerTransform.transform.position);
                 }
-                else if (isFighting)
+                if(isGoingOnPlayer)
                 {
-                    isFighting = false;
+                    isGoingOnPlayer = false;
                 }
             }
 
@@ -145,14 +132,58 @@ public class Range : Mobs, IRange
         }
     }
 
-    private void LongRangeAttack(Vector3 positionToReach)
+    private IEnumerator LongRangeAttack(Transform player)
     {
+        // Stop bomber's attack
+        canAttack = false;
+        isAttacking = true;
+        // Wait to face player
         GameObject bomb = Instantiate(pfBomb, this.transform.position, Quaternion.identity);
         ExplodingBomb exploBomb = bomb.GetComponent<ExplodingBomb>();
         float timeToThrow = 0.8f;
+        Vector3 positionToReach = player.position;
         StartCoroutine(exploBomb.ThrowToPos(positionToReach, timeToThrow));
         exploBomb.SetTimeToExplode(timeToThrow * 1.5f);
         exploBomb.Activate();
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
+        yield return new WaitForSeconds(1f);
+        canAttack = true;
+    }
+
+    public Vector2 GetPointOnCircle(Vector2 center, float radius)
+    {
+        float randomValue = Random.Range(0, 2 * Mathf.PI);
+        return new Vector2(center.x + Mathf.Cos(randomValue) * radius, center.y + Mathf.Sin(randomValue) * radius);
+    }
+    public IEnumerator FaceToPlayer(Transform player)
+    {
+        while (!IsTurnToPlayer())
+        {
+            yield return null;
+            this.transform.LookAt(player.transform.position);
+            //float timer = 0f;
+            //Vector3 playerPos = player.position;
+            //Vector3 targetToThis = (playerPos - this.transform.position).normalized;
+            //Quaternion rotation;
+            //Vector3 baseRotation = this.transform.eulerAngles;
+            //Vector3 finalRotation = baseRotation;
+            //if (targetToThis != Vector3.zero)
+            //{
+            //    rotation = Quaternion.LookRotation(new Vector3(targetToThis.x, 0, targetToThis.z));
+            //    rotation *= Camera.main.transform.rotation;
+            //    float rotY = rotation.eulerAngles.y;
+
+            //    finalRotation.y = rotY;
+            //}
+            //while (timer < 1f)
+            //{
+            //    this.transform.eulerAngles = Vector3.Lerp(baseRotation, finalRotation, timer);
+            //    timer += Time.deltaTime * 3;
+            //    yield return null;
+            //}
+        }
+
     }
 
     public void ApplyDamage(int _value, bool isCrit = false, bool hasAnimation = true)
@@ -183,7 +214,11 @@ public class Range : Mobs, IRange
     //        state = RangeState.WANDERING;
     //    }
     //}
-
+    public List<Vector3> GetDashesPath(Vector3 posToReach, int nbDash)
+    {
+        List<Vector3> path = new List<Vector3>();
+        return new List<Vector3>();
+    }
     public void Death()
     {
         OnDeath?.Invoke(transform.position);
@@ -204,26 +239,26 @@ public class Range : Mobs, IRange
     }
 
 #if UNITY_EDITOR
-    private void DisplayFleeingRange(float _angle)
-    {
-        Handles.color = new Color(1, 1, 0f, 0.25f);
-        Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, _angle / 2f, FleeingRange);
-        Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -_angle / 2f, FleeingRange);
+    //private void DisplayFleeingRange(float _angle)
+    //{
+    //    Handles.color = new Color(1, 1, 0f, 0.25f);
+    //    Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, _angle / 2f, FleeingRange);
+    //    Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -_angle / 2f, FleeingRange);
 
-        Handles.color = Color.white;
-        Handles.DrawWireDisc(transform.position, Vector3.up, FleeingRange);
-    }
+    //    Handles.color = Color.white;
+    //    Handles.DrawWireDisc(transform.position, Vector3.up, FleeingRange);
+    //}
 
-    private void OnDrawGizmos()
-    {
-        if (!Selection.Contains(gameObject))
-            return;
+    //private void OnDrawGizmos()
+    //{
+    //    if (!Selection.Contains(gameObject))
+    //        return;
 
-        DisplayVisionRange(isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS));
-        DisplayAttackRange(isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS));
-        DisplayFleeingRange(isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS));
-        DisplayInfos();
-    }
+    //    DisplayVisionRange(isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS));
+    //    DisplayAttackRange(isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS));
+    //    DisplayFleeingRange(isFighting ? 360 : (int)stats.GetValue(Stat.CATCH_RADIUS));
+    //    DisplayInfos();
+    //}
 #endif
 }
 
