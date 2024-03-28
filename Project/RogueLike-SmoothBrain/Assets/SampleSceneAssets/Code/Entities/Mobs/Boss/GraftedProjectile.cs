f279aa51bb936cf7c378509bf2a4c7301554eed5
yhnnlr <154349665+yhnnlr@users.noreply.github.com>
2024-03-28 16:54:24 +0100

    compétences du boss finies

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Boss/GraftedProjectile.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Boss/GraftedProjectile.cs
@@ -1,10 +1,15 @@
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class GraftedProjectile : Projectile
 {
     [HideInInspector]
     public bool onTarget = false;
+    bool ignoreCollisions = false;
     Vector3 direction;
+    Grafted grafted;
+
+    public void SetCollisionImmune(bool _state) { ignoreCollisions = _state; }
 
     protected override void Awake()
     {
@@ -12,7 +17,12 @@ public class GraftedProjectile : Projectile
         base.Awake();
     }
 
-    public void Initialize(Vector3 _direction)
+    public void Initialize(Grafted _grafted)
+    {
+        grafted = _grafted;
+    }
+    
+    public void SetDirection(Vector3 _direction)
     {
         direction = _direction;
     }
@@ -25,19 +35,34 @@ public class GraftedProjectile : Projectile
         }
     }
 
+    public bool OnLauncher(Vector3 _launcher)
+    {
+        return Vector3.Distance(transform.position, _launcher) < 1f;
+    }
+
     protected override void OnTriggerEnter(Collider other)
     {
-        if (((1 << other.gameObject.layer) & LayerMask.GetMask("Map")) != 0 && !other.isTrigger)
+        if (!ignoreCollisions)
         {
-            onTarget = true;
-            return;
+            if (((1 << other.gameObject.layer) & LayerMask.GetMask("Map")) != 0 && !other.isTrigger)
+            {
+                onTarget = true;
+                return;
+            }
         }
 
         IDamageable damageableObject = other.GetComponent<IDamageable>();
         if (damageableObject != null && !onTarget)
         {
             damageableObject.ApplyDamage(damage);
-            direction = -Vector3.up;
+            if (!ignoreCollisions)
+            {
+                direction = -Vector3.up;
+            }
+            else
+            {
+                grafted.ApplyKnockback(damageableObject, new Vector3(-direction.z, 0, direction.x).normalized);
+            }
         }
     }
 }
