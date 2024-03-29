f033b96a1a93193f54ec223e30ca61b3fdc09847
Dorian <thesothief@gmail.com>
2024-03-29 15:53:44 +0100

    Nathan recommit par dessus moi et conséquence

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/GamepadIcons.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/GamepadIcons.cs
@@ -137,5 +137,44 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 return null;
             }
         }
+
+        [Serializable]
+        public struct KeyboardIconsSprites
+
+        {
+            public Sprite buttonLong;
+            public Sprite buttonMedium;
+            public Sprite buttonSimple;
+            public Sprite buttonCorner;
+
+            public Sprite GetSprite(string controlPath)
+            {
+                // From the input system, we get the path of the control on device. So we can just
+                // map from that to the sprites we have for gamepads.
+                switch (controlPath)
+                {
+                    case "buttonSouth": return buttonSouth;
+                    case "buttonNorth": return buttonNorth;
+                    case "buttonEast": return buttonEast;
+                    case "buttonWest": return buttonWest;
+                    case "start": return startButton;
+                    case "select": return selectButton;
+                    case "leftTrigger": return leftTrigger;
+                    case "rightTrigger": return rightTrigger;
+                    case "leftShoulder": return leftShoulder;
+                    case "rightShoulder": return rightShoulder;
+                    case "dpad": return dpad;
+                    case "dpad/up": return dpadUp;
+                    case "dpad/down": return dpadDown;
+                    case "dpad/left": return dpadLeft;
+                    case "dpad/right": return dpadRight;
+                    case "leftStick": return leftStick;
+                    case "rightStick": return rightStick;
+                    case "leftStickPress": return leftStickPress;
+                    case "rightStickPress": return rightStickPress;
+                }
+                return null;
+            }
+        }
     }
 }
