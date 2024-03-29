12db923e2444275e4795acf9d361176f69e1ceb5
Berti Enzo <144116555+EnzoTatou@users.noreply.github.com>
2024-03-29 15:09:10 +0100

    a

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Objects/DungeonGate.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Objects/DungeonGate.cs
@@ -3,16 +3,33 @@ using UnityEngine;
 
 public class DungeonGate : MonoBehaviour
 {
-    private Material material;
+    [SerializeField] private Material material;
+    [SerializeField] private BoxCollider boxCollider;
+
     private void Awake()
     {
-        material = GetComponent<Renderer>().material;
+        // set value to default
+        material.SetFloat("_Dissolve", 0f);
+
         RoomUtilities.EnterEvents += Close;
+        RoomUtilities.allEnemiesDeadEvents += Open;
+    }
+
+    private void Open()
+    {
+        StartCoroutine(SetDisolve(0f));
+        boxCollider.enabled = false;
     }
 
     private void Close()
     {
+        if (RoomUtilities.roomData.Type == RoomType.Lobby)
+        {
+            return;
+        }
+
         StartCoroutine(SetDisolve(1f));
+        boxCollider.enabled = true;
     }
 
     IEnumerator SetDisolve(float desiredDisolve)
