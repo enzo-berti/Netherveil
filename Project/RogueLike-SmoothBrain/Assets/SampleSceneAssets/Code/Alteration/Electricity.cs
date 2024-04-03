d7957be854b4e20a2fa1f9e0710b6d5f191dd28e
Berti Enzo <144116555+EnzoTatou@users.noreply.github.com>
2024-04-03 16:27:07 +0200

    Update Electricity.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Alteration/Electricity.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Alteration/Electricity.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.VFX.Utility;
+using UnityEngine.VFX;
 
 public class Electricity : Status
 {
     private float entityBaseSpeed;
+    VisualEffect vfx;
+
     public Electricity(float duration = 1f) : base(duration)
     {
         this.isConst = true;
@@ -14,7 +18,10 @@ public class Electricity : Status
         {
             target.AddStatus(this);
             entityBaseSpeed = target.Stats.GetValue(Stat.SPEED);
-            GameObject.Instantiate(Resources.Load<GameObject>("VFX_Electricity"), target.transform.parent);
+            vfx = GameObject.Instantiate(Resources.Load<GameObject>("VFX_Electricity")).GetComponent<VisualEffect>();
+            vfx.SetSkinnedMeshRenderer("New SkinnedMeshRenderer", target.gameObject.GetComponentInChildren<SkinnedMeshRenderer>());
+            vfx.GetComponent<VFXPropertyBinder>().GetPropertyBinders<VFXTransformBinderCustom>().ToArray()[0].Target = target.gameObject.GetComponentInChildren<VFXTarget>().transform;
+            vfx.Play();
         }
     }
 
@@ -26,6 +33,7 @@ public class Electricity : Status
     public override void OnFinished()
     {
         target.Stats.SetValue(Stat.SPEED, entityBaseSpeed);
+        GameObject.Destroy(vfx.gameObject);
     }
 
     protected override void Effect()
