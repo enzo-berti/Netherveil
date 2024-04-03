0886189951cad53cc6948d16ab099ebce2025490
EnzoTatou <enzoberti@protonmail.com>
2024-04-03 23:04:33 +0200

    Update MapGenerator.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Map/MapGenerator.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Map/MapGenerator.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.AI.Navigation;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.ProBuilder.Shapes;
 
 public enum RoomType
 {
