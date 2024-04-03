da0ce32dbfe2ad7d36c8dbe8625b17a5781f7d7c
Léo <leo413568@gmail.com>
2024-04-03 15:33:15 +0200

    Update MouseOnScreenBorder.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Camera/MouseOnScreenBorder.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Camera/MouseOnScreenBorder.cs
@@ -6,7 +6,7 @@ public class MouseOnScreenBorder : MonoBehaviour
 {
     private Vector3 targetPosition;
     private Vector3 currentVelocity = Vector3.zero;
-    private float smoothTime = 0.2f; 
+    private float smoothTime = 0.2f;
     private Transform playerTransform;
     private PlayerInput playerInput;
     private PlayerInputMap inputs;
@@ -37,19 +37,16 @@ public class MouseOnScreenBorder : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (player.State == (int)EntityState.DEAD)
+        if (DeviceManager.Instance.IsPlayingKB())
         {
-            if (DeviceManager.Instance.IsPlayingKB())
-            {
-                CollidMouseScreen();
-            }
-            else
-            {
-                CollideJoystickScreen();
-            }
-
-            ChangeOffsetPos();
+            CollidMouseScreen();
         }
+        else
+        {
+            CollideJoystickScreen();
+        }
+
+        ChangeOffsetPos();
     }
 
     private void CollidMouseScreen()
@@ -57,7 +54,7 @@ public class MouseOnScreenBorder : MonoBehaviour
         Vector2 mousepos = Input.mousePosition;
         Vector3 offsetX = Vector3.zero;
         Vector3 offsetY = Vector3.zero;
-        float offsetDistBorder = 10f; 
+        float offsetDistBorder = 10f;
         float offsetDistCam = 2f;
 
         if (mousepos.x > Screen.width - offsetDistBorder)
@@ -111,13 +108,13 @@ public class MouseOnScreenBorder : MonoBehaviour
 
     private void ChangeOffsetPos()
     {
-        if (targetPosition != playerTransform.position && playerInput.Direction == Vector2.zero)
+        if (targetPosition != playerTransform.position && playerInput.Direction == Vector2.zero && player.State != (int)EntityState.DEAD)
         {
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
         }
         else
         {
-            transform.position = Vector3.SmoothDamp(transform.position, playerTransform.position, ref currentVelocity, smoothTime/3);
+            transform.position = Vector3.SmoothDamp(transform.position, playerTransform.position, ref currentVelocity, smoothTime / 3);
         }
     }
 }
