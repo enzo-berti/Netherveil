ec7be1878a51efa1c788ac9607767ca60f54cd05
Berti Enzo <144116555+EnzoTatou@users.noreply.github.com>
2024-04-04 09:37:10 +0200

    AAA

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/WinScreen.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/WinScreen.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour
 {
+    public static WinScreen instance;
     [SerializeField] private Image blackPanel;
     [SerializeField] private Image buttonPanel;
     [SerializeField] private Image menu;
@@ -15,6 +16,12 @@ public class WinScreen : MonoBehaviour
     [SerializeField] private TextMeshProUGUI EndText;
     [SerializeField] private TextMeshProUGUI EndText2;
     [SerializeField] private TextMeshProUGUI EndText3;
+
+    private void Awake()
+    {
+        instance = this;
+        gameObject.SetActive(false);
+    }
 
     private void OnEnable()
     {
