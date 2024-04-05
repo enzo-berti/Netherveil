273f201f07eb084d99c378a023e8a8b5792d93b1
Toraenor <olivier.maurin.development@gmail.com>
2024-04-05 19:24:28 +0200

    Update SneakyDagger.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/SneakyDagger.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/SneakyDagger.cs
@@ -18,7 +18,8 @@ public class SneakyDagger : ItemEffect , IPassiveItem
         Transform player = GameObject.FindWithTag("Player").transform;
         Vector3 enemyToPlayerVec = (player.position - (target as MonoBehaviour).transform.position).normalized;
 
-        //if the player is in the back of the enemy, and is in angle behind of 2 * (180 - (180 * 0.85)), it inflicts more damages
+        //if the player is in the back of the enemy, and is in an angle behind of 2 * (180 - (180 * 0.85)), it inflicts more damages
+        //*2 because it is mirrored based on opposite of forward
         if (Vector3.Dot(enemyToPlayerVec, (target as MonoBehaviour).transform.forward) < -0.85f)
         {
             damages += attackValue;
