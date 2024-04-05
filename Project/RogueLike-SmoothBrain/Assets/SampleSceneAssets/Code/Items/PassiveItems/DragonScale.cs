fcaddcb7e14d6462fdd984db81fa8b2819bc2be6
Toraenor <olivier.maurin.development@gmail.com>
2024-04-05 16:03:01 +0200

    new items! :bomb:

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/DragonScale.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/DragonScale.cs
@@ -1,15 +1,23 @@
 using UnityEngine; 
  
 public class DragonScale : ItemEffect , IPassiveItem 
-{ 
+{
+    readonly float coefValue = 0.5f;
+    readonly float hpValue = 25f;
     public void OnRetrieved() 
     { 
-        throw new System.NotImplementedException(); 
+        Hero hero = GameObject.FindWithTag("Player").GetComponent<Hero>();
+        hero.Stats.IncreaseCoeffValue(Stat.ATK, coefValue);
+        hero.Stats.IncreaseMaxValue(Stat.HP, hpValue);
+        hero.Stats.IncreaseValue(Stat.HP, hpValue);
     } 
  
     public void OnRemove() 
-    { 
-        throw new System.NotImplementedException(); 
+    {
+        Hero hero = GameObject.FindWithTag("Player").GetComponent<Hero>();
+        hero.Stats.DecreaseCoeffValue(Stat.ATK, coefValue);
+        hero.Stats.DecreaseMaxValue(Stat.HP, hpValue);
+        hero.Stats.DecreaseValue(Stat.HP, hpValue);
     } 
  
 } 
