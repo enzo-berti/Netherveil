bbc46b065ebd43ae7e52292b2aa7934e58418ec0
Toraenor <olivier.maurin.development@gmail.com>
2024-04-08 18:31:03 +0200

    Update SpearGhosts.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/SpearGhosts.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/SpearGhosts.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpearGhosts : ItemEffect , IPassiveItem 
@@ -8,7 +7,6 @@ public class SpearGhosts : ItemEffect , IPassiveItem
     readonly List<GameObject> spearThrowWrappers = new();
     public void OnRetrieved() 
     {
-        //instantiate as well 2 more spearThrowColliders
         //used a wrapper instead of the object itself to make it rotate from player's position, not the middle of the collide
         GameObject spearThrowWrapper = GameObject.FindWithTag("Player").GetComponent<PlayerController>().SpearThrowWrapper;
 
@@ -39,19 +37,14 @@ public class SpearGhosts : ItemEffect , IPassiveItem
  
     private void ThrowSpearGhosts(Vector3 posToReach)
     {
-        //instantiate 2 spears and stock them into a list
         //if possible instantiate them with a different material to differentiate them from the original one
-
-        Transform spearTransform = GameObject.FindWithTag("Player").GetComponent<PlayerController>().Spear.transform;
+        GameObject spear = GameObject.FindWithTag("Player").GetComponent<PlayerController>().Spear.gameObject;
         for (int i = 0; i< 2; ++i)
         {
-            ghostSpears.Add(GameObject.Instantiate(Resources.Load<GameObject>("Spear"), spearTransform.position, spearTransform.rotation, spearTransform.parent));
+            ghostSpears.Add(GameObject.Instantiate(spear, spear.transform.position, spear.transform.rotation, spear.transform.parent));
         }
 
-        //rotate posToReach by 30° and -30° for spear 1 and 2 (need to take into consideration camera angles)
-        //assign the spearThrowColliders to the spears
-        //throw them
-
+        //need to take into consideration camera angles
         for (int i = 0; i< ghostSpears.Count; ++i)
         {
             ghostSpears[i].GetComponent<Spear>().SpearThrowCollider = spearThrowWrappers[i].GetComponentInChildren<BoxCollider>(includeInactive: true);
@@ -63,7 +56,6 @@ public class SpearGhosts : ItemEffect , IPassiveItem
 
     private void RetrieveSpearGhosts()
     {
-        //retrieve the 2 stocked spears
         foreach(GameObject spear in ghostSpears)
         {
             spear.GetComponent<Spear>().Return();
@@ -72,7 +64,6 @@ public class SpearGhosts : ItemEffect , IPassiveItem
 
     private void DestroySpearGhosts()
     {
-        //destroy the 2 spear ghosts and clear list
         foreach(GameObject spear in ghostSpears)
         {
             GameObject.Destroy(spear);
