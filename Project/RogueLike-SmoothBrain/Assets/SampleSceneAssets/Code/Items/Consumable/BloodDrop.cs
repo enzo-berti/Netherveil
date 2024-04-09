72d3fb0bd995c806b96732d009dbcb2644ffc167
Toraenor <olivier.maurin.development@gmail.com>
2024-04-09 22:14:26 +0200

    some cleaning

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/Consumable/BloodDrop.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/Consumable/BloodDrop.cs
@@ -5,11 +5,6 @@ public class BloodDrop : Consumable
     public static float BloodDropCoeff = 1.0f;
     [SerializeField] int bloodQuantity = 0;
 
-    protected override void Start()
-    {
-        base.Start();
-    }
-
     public override void OnRetrieved()
     {
         player.Inventory.Blood += (int)(bloodQuantity * BloodDropCoeff);
