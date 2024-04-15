23891f1a957b9d3582eda48c4358d7edbf46b703
Dorian <thesothief@gmail.com>
2024-04-15 15:42:33 +0200

    Settings

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/Menu/Widgets/ScrollRectExtension.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollRectExtension : MonoBehaviour
+{
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private RectTransform contentPanel;
+    [SerializeField] private float offsetY = 25f;
+
+    private void Awake()
+    {
+        Selectable[] selectables = GetComponentsInChildren<Selectable>(true);
+        foreach (Selectable selectable in selectables)
+        {
+            if (!selectable.TryGetComponent(out EventTrigger trigger))
+                trigger = selectable.AddComponent<EventTrigger>();
+
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.Select;
+            entry.callback.AddListener(d => Debug.Log("aled"));
+            entry.callback.AddListener(OnSelect);
+            trigger.triggers.Add(entry);
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("t");
+        SnapTo(eventData.selectedObject.GetComponent<RectTransform>());
+    }
+
+    public void SnapTo(RectTransform target)
+    {
+        Canvas.ForceUpdateCanvases();
+
+        float y = scrollRect.transform.InverseTransformPoint(contentPanel.position).y - scrollRect.transform.InverseTransformPoint(target.position).y;
+        contentPanel.anchoredPosition = new Vector2(0, y - offsetY);
+    }
+}
