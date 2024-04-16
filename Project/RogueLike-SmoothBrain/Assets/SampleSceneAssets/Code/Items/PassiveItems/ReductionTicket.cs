a8921693bdfa9a76f884c0dc3f6938b623509660
Toraenor <olivier.maurin.development@gmail.com>
2024-04-16 18:57:09 +0200

    small cleaning and new item

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/ReductionTicket.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/PassiveItems/ReductionTicket.cs
@@ -1,15 +1,29 @@
 using UnityEngine; 
  
 public class ReductionTicket : ItemEffect , IPassiveItem 
-{ 
+{
+    readonly float coefValue = 0.5f;
+
+#pragma warning disable CS0414 // Supprimer le warning dans unity
+#pragma warning disable IDE0052 // Supprimer les membres privés non lus
+    //used to display in description, dont delete it
+    private readonly float displayValue = 0f;
+#pragma warning restore IDE0052
+#pragma warning restore CS0414
+
+    public ReductionTicket()
+    {
+        displayValue = coefValue * 100f;
+    }
+
     public void OnRetrieved() 
     {
-        //throw new System.NotImplementedException(); 
+        Item.priceCoef *= coefValue;
     }
 
     public void OnRemove() 
-    { 
-        throw new System.NotImplementedException(); 
+    {
+        Item.priceCoef /= coefValue;
     } 
  
 } 
