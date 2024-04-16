c1b302a8165acd97f0027e0a7dde3fc42d4ea804
Toraenor <olivier.maurin.development@gmail.com>
2024-04-16 09:56:06 +0200

    quest structure finished

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Npc/Quest/Restrictive/RealTest.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Npc/Quest/Restrictive/RealTest.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class RealTest : Quest
 {
     int currentNumber = 0;
-    readonly int MAX_NUMBER = 25;
+    readonly int MAX_NUMBER = 1;
 
     public override void AcceptQuest()
     {
@@ -22,5 +22,10 @@ public class RealTest : Quest
         currentNumber++;
         progressText = $"NB ENEMIES KILLED : {currentNumber}/{MAX_NUMBER}";
         QuestUpdated();
+
+        if(currentNumber >= MAX_NUMBER) 
+        {
+            QuestFinished();
+        }
     }
 } 
