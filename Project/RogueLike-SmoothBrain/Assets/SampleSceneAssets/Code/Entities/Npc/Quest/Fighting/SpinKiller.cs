d0e384328d2e08c421f2ecce8e8f7d6ac0ee1640
Léo <leo413568@gmail.com>
2024-04-16 15:28:52 +0200

    add new quest

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Npc/Quest/Fighting/SpinKiller.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+
+public class SpinKiller : Quest
+{
+    int currentNumber = 0;
+    bool asDoAnChargedAttack = false;
+    readonly int MAX_NUMBER = 5;
+
+    public override void AcceptQuest()
+    {
+        progressText = $"NB ENEMIES KILL WITH CHARGED ATTACK: {currentNumber}/{MAX_NUMBER}";
+        Hero.OnChargedAttack += SetBool;
+        GameObject.FindWithTag("Player").GetComponent<Hero>().OnAttackHit += UpdateCount;
+    }
+
+    protected override void QuestFinished()
+    {
+        base.QuestFinished();
+        Hero.OnChargedAttack -= SetBool;
+        GameObject.FindWithTag("Player").GetComponent<Hero>().OnAttackHit -= UpdateCount;
+    }
+
+    private void SetBool(IDamageable damageable, IAttacker attacker)
+    {
+        asDoAnChargedAttack = true;
+    }
+
+    private void UpdateCount(IDamageable damageable, IAttacker attacker)
+    {
+        Entity monster = (damageable as Entity);
+        if (asDoAnChargedAttack && monster != null && monster.Stats.GetValue(Stat.HP) <= 0)
+        {
+            currentNumber++;
+            progressText = $"NB ENEMIES KILL WITH CHARGED ATTACK : {currentNumber}/{MAX_NUMBER}";
+            QuestUpdated();
+
+            if (currentNumber >= MAX_NUMBER)
+            {
+                QuestFinished();
+            }
+        }
+        asDoAnChargedAttack = false;
+    }
+}
