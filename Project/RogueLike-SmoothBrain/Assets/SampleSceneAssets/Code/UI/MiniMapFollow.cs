6d3b32b3992b7d20c8c936f9858813bc909145d9
Berti Enzo <144116555+EnzoTatou@users.noreply.github.com>
2024-04-17 17:00:24 +0200

    Je travail pour les graphs no way

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/MiniMapFollow.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/MiniMapFollow.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class MiniMapFollow : MonoBehaviour
 {
+    private void Start()
+    {
+        
+    }
+
     private void Update()
     {
         transform.forward = Vector3.right;
