63cdd1c1ca154e2640d2566b57c240a0e380931c
Dorian <thesothief@gmail.com>
2024-04-18 08:49:20 +0200

    Converter script

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Converter/ConvertUrpToToon.cs
@@ -0,0 +1,39 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class ConvertUrpToToon
+{
+    public static Shader toonShader;
+
+    [MenuItem("Tools/Convert Materials to ToonShader")]
+    public static void ConvertMaterialsToToonShader()
+    {
+        string[] allMaterialGUIDs = AssetDatabase.FindAssets("t:Material");
+        Shader toonShader = Shader.Find("Toon");
+
+        foreach (string materialGUID in allMaterialGUIDs)
+        {
+            string materialPath = AssetDatabase.GUIDToAssetPath(materialGUID);
+            Material material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+
+            if (material != null)
+            {
+                if (!material.shader.name.Contains("Toon"))
+                {
+                    if (toonShader != null)
+                    {
+                        material.shader = toonShader;
+                        EditorUtility.SetDirty(material);
+                    }
+                    else
+                    {
+                        Debug.LogError("ToonShader not found. Make sure the ToonShader is in your project.");
+                    }
+                }
+            }
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
