6be9bdf90542153634e3bb608d218164bb1f4481
yhnnlr <154349665+yhnnlr@users.noreply.github.com>
2024-04-18 09:45:29 +0200

    removed debug log

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Gorgon/States/GorgonTriggeredState.cs
@@ -0,0 +1,51 @@
+// ---[ STATE ] ---
+// replace "GorgonTriggeredState_STATEMACHINE" by your state machine class name.
+//
+// Here you can see an exemple of the CheckSwitchStates method:
+// protected override void CheckSwitchStates()
+// {
+//      if (isRunning)
+//      {
+//          SwitchState(Factory.GetState<RunningState>());
+//      }
+// }
+
+using StateMachine; // include all scripts about StateMachines
+
+public class GorgonTriggeredState : BaseState<GorgonStateMachine>
+{
+    public GorgonTriggeredState(GorgonStateMachine currentContext, StateFactory<GorgonStateMachine> currentFactory)
+        : base(currentContext, currentFactory) { }
+        
+    // This method will be called every Update to check whether or not to switch states.
+    protected override void CheckSwitchStates()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // This method will be called only once before the update.
+    protected override void EnterState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // This method will be called only once after the last update.
+    protected override void ExitState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // This method will be called every frame.
+    protected override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // This method will be called on state switch.
+    // No need to modify this method !
+    protected override void SwitchState(BaseState<GorgonStateMachine> newState)
+    {
+        base.SwitchState(newState);
+        Context.currentState = newState;
+    }
+}
