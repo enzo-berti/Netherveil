deeab53c1cb04ab0fcb27d571a4c74121c11e9a6
Dorian <thesothief@gmail.com>
2024-04-19 11:12:43 +0200

    floating text

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Foutains/Fountain.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Foutains/Fountain.cs
@@ -18,5 +18,6 @@ namespace Fountain
         public int BloodPrice => bloodPrice;
         public int ValueTrade => type == FountainType.Corruption ? valueTrade : -valueTrade;
         public int AbsoluteValueTrade => valueTrade;
+        public Color Color => type == FountainType.Corruption ? new Color(0.62f, 0.34f, 0.76f, 1.0f) : new Color(0.0f, 0.94f, 1.0f, 1.0f);
     }
 }
