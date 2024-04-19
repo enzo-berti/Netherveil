308198667ee430296d367d399262160ef18d4057
Dorian <thesothief@gmail.com>
2024-04-19 18:08:45 +0200

    Buffs

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/HUD/BuffHUD.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuffHUD : MonoBehaviour
+{
+    [SerializeField] private GameObject buffIconPrefab;
+    [SerializeField] private Transform buffTransform;
+
+    public void AddBuffIcon(Texture texture)
+    {
+        GameObject buff = Instantiate(buffIconPrefab, buffTransform);
+        buffTransform.GetComponent<RawImage>().texture = texture;
+    }
+}
