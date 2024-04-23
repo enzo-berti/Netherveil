793e9eef5c6b30f96866acf479aab1341bac3cd6
yhnnlr <154349665+yhnnlr@users.noreply.github.com>
2024-04-23 16:51:38 +0200

    buff pests

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Pest/PestStateMachine.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Pest/PestStateMachine.cs
@@ -55,7 +55,7 @@ public class PestStateMachine : Mobs, IPest
     public float VisionAngle { get => (currentState is PestTriggeredState || currentState is PestAttackingState) && Player != null ? 360 : angle; }
     public float VisionRange { get => Stats.GetValue(Stat.VISION_RANGE) * (currentState is PestTriggeredState || currentState is PestAttackingState ? 1.25f : 1f); }
     public float MovementTimer { set => dashTimer = value; }
-    public float MovementDelay { get => (currentState is PestTriggeredState ? 1f : 1.5f); }
+    public float MovementDelay { get => (currentState is PestTriggeredState ? 0.75f : 1f); }
     public bool CanMove { get => dashTimer > MovementDelay; }
 
     protected override void Start()
