920a626eafe2b957709d7a94c3c3490060dd4ef3
Dorian <thesothief@gmail.com>
2024-04-23 10:42:47 +0200

    PostProcessing manager

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/PostProcessing/Effects/PostProcessingHitEffect.cs
@@ -0,0 +1,39 @@
+using PostProcessingEffects.Effects;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PostProcessingEffects
+{
+    [System.Serializable]
+    public class PostProcessingHitEffect : PostProcessingAbstractEffect
+    {
+        [SerializeField] private VolumeProfile profile;
+        [SerializeField] private float duration = 0.5f;
+
+        protected override IEnumerator PlayRoutine(PostProcessingEffectManager manager)
+        {
+            manager.Volume.profile = profile;
+            float elapsed = 0.0f;
+
+            while (elapsed < duration)
+            {
+                elapsed = Mathf.Min(elapsed + Time.deltaTime, duration);
+                float factor = elapsed / duration;
+                float ease = Mathf.Sin(factor * Mathf.PI);
+
+                manager.Volume.weight = ease;
+
+                yield return null;
+            }
+
+            manager.Volume.weight = 0.0f;
+            manager.routine = null;
+        }
+
+        protected override IEnumerator StopRoutine(PostProcessingEffectManager manager)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
