4e3075d80a33c3d458ea84a315151f13cc2b74fc
naoki18 <jadras.dunkan@gmail.com>
2024-04-23 09:29:36 +0200

    Update Fire.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Alteration/Fire.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Alteration/Fire.cs
@@ -7,7 +7,6 @@ public class Fire : OverTimeStatus
     private int damage = 10;
     static Color fireColor = new Color(0.929f, 0.39f, 0.08f, 1.0f);
     public static event Action OnFire;
-    public bool firstEffect = true;
     public Fire(float _duration, float _statusChance) : base(_duration, _statusChance)
     {
         frequency = 0.5f;
@@ -15,11 +14,6 @@ public class Fire : OverTimeStatus
     }
     protected override void Effect()
     {
-        if(firstEffect)
-        {
-            firstEffect = false;
-            OnFire?.Invoke();
-        }
         if (target != null)
         {
             FloatingTextGenerator.CreateEffectDamageText(damage * Stack, target.transform.position, fireColor);
@@ -44,6 +38,7 @@ public class Fire : OverTimeStatus
 
     protected override void PlayStatus()
     {
+        OnFire?.Invoke();
         PlayVfx("VFX_Fire");
     }
 }
