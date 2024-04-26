0c0b34369d669f7548bc6c8c65783652108b75af
Dorian <thesothief@gmail.com>
2024-04-26 12:40:04 +0200

    videoPlayer

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/HUD/DescriptionTabHUD.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/HUD/DescriptionTabHUD.cs
@@ -1,16 +1,19 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class DescriptionTabHUD : MonoBehaviour
 {
     [SerializeField] private RectTransform tabRectTransform;
     [SerializeField] private TMP_Text titleTextMesh;
     [SerializeField] private TMP_Text descriptionTextMesh;
+    [SerializeField] private VideoPlayer videoPlayer;
 
-    public void SetTab(string title, string description)
+    public void SetTab(string title, string description, VideoClip clip)
     {
         titleTextMesh.text = title;
         descriptionTextMesh.text = description;
+        videoPlayer.clip = clip;
     }
 
     public void OpenTab()
