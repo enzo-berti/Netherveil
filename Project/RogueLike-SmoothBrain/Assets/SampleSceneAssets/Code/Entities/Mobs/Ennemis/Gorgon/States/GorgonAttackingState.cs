1c327163f19aff2c8d3863dc01fb95c58e7258c0
yhnnlr <154349665+yhnnlr@users.noreply.github.com>
2024-04-29 22:09:33 +0200

    fix nullref gorgon

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Gorgon/States/GorgonAttackingState.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Gorgon/States/GorgonAttackingState.cs
@@ -49,11 +49,14 @@ public class GorgonAttackingState : BaseState<GorgonStateMachine>
     // This method will be called every frame.
     protected override void UpdateState()
     {
-        Quaternion lookRotation = Quaternion.LookRotation(Context.Player.transform.position - Context.transform.position);
-        lookRotation.x = 0;
-        lookRotation.z = 0;
+        if (Context.Player)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(Context.Player.transform.position - Context.transform.position);
+            lookRotation.x = 0;
+            lookRotation.z = 0;
 
-        Context.transform.rotation = Quaternion.Slerp(Context.transform.rotation, lookRotation, 10f * Time.deltaTime);
+            Context.transform.rotation = Quaternion.Slerp(Context.transform.rotation, lookRotation, 10f * Time.deltaTime);
+        }
     }
 
     // This method will be called on state switch.
