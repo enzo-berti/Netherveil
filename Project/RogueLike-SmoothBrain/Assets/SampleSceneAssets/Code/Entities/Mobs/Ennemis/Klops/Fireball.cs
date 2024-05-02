b159ca4eec49d206dcebef33aefcd5bddf6a6fac
yhnnlr <154349665+yhnnlr@users.noreply.github.com>
2024-05-02 12:48:22 +0200

    klops fixed

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Klops/Fireball.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Ennemis/Klops/Fireball.cs
@@ -11,18 +11,36 @@ public class Fireball : MonoBehaviour, IAttacker
     public List<Status> StatusToApply => statusToApply;
     public float FireballSpeed = 2f;
 
+    float radius = 0f;
+
     public IAttacker.AttackDelegate OnAttack { get; set; }
     public IAttacker.HitDelegate OnAttackHit { get; set; }
 
     void Start()
     {
         Destroy(gameObject, 2.0f);
+
+        //radius = GetComponentInChildren<Renderer>().bounds.size.y / 2f;
+        radius = GetComponent<CapsuleCollider>().radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += direction.normalized * Time.deltaTime * FireballSpeed;
+        transform.position += direction.normalized * Time.deltaTime * FireballSpeed;
+
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, radius);
+        if (hits.Length > 0)
+        {
+            foreach (var hit in hits)
+            {
+                if (((1 << hit.collider.gameObject.layer) & LayerMask.GetMask("Map")) != 0)
+                {
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
