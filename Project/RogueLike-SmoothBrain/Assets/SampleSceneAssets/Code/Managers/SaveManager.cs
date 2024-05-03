138cc90ec9c0b76980e5649781d6fcf656f7d2ba
EnzoTatou <enzoberti@protonmail.com>
2024-05-03 20:47:40 +0200

    Add SaveManager

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/SaveManager.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveManager : MonoBehaviour
+{
+    static private SaveManager instance;
+    static public SaveManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject obj = new GameObject(nameof(SaveManager));
+                obj.AddComponent<SaveManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    public Action onSave; 
+
+    private int selectedSave = -1;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(instance);
+            return;
+        }
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level != SceneManager.GetSceneByName("InGame").buildIndex || selectedSave < 0)
+        {
+            return;
+        }
+
+
+    }
+
+    public bool SelectSave(int saveSelected)
+    {
+
+        return false;
+    }
+
+    private void Load(int saveIndex)
+    {
+
+    }
+
+    public void Save()
+    {
+        onSave?.Invoke();
+    }
+}
