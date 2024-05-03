86d89e5893e96eddb99211cc241defca4a6bc00c
naoki18 <jadras.dunkan@gmail.com>
2024-05-03 16:06:02 +0200

    fix

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/GeneralEntities/DropInfo.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/GeneralEntities/DropInfo.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityEngine.Events;
 
 [Serializable]
 public class DropInfo
@@ -10,17 +9,12 @@ public class DropInfo
     [Range(0.0f, 1.0f)]
     public float chance;
 
-    public int Quantity;
+    public int quantity;
 
     [Tooltip("If chance is shared that means all quantity will drop with one chance shared.\n\n" +
         "For exemple an item ( loot = potion, chance = 0.5, quantity = 2 ) with isChanceShared true. This item has 50% chance to drop two instances of the item and can't drop only one\n\n" +
         "but if isChanceShared false, each potion has 50% chance to drop. So, for a quantity of 2, there is 25% ( 50% * 50% ) chance to drop")]
     public bool isChanceShared;
-    public bool isChanceFromFunction;
-    public UnityEvent<float> FunctionToFollow;
+    public float decreasingValuePerDrop;
 
-    public void test()
-    {
-       float test =  FunctionToFollow.Invoke();
-    }
 }
