fb00f46113ec24a4de3ca4f507cee05943931faa
Nicromell <71634044+Nicromell@users.noreply.github.com>
2024-05-06 21:48:55 +0200

    je commit un crash unity xd

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Levels/Prefabs/RoomBuilder/EEPrefabs/RecordPlayer.cs
@@ -0,0 +1,68 @@
+using FMOD.Studio;
+using FMODUnity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Serialization;
+using UnityEngine;
+
+public class RecordPlayer : MonoBehaviour
+{
+
+    public EventReference AllMyTearsMusic;
+    [SerializeField] ParticleSystem MusicNote;
+    bool IsCollide;
+    bool IsMusicPlaying = false;
+    EventInstance eventMusic;
+
+    void Start()
+    {
+        MusicNote.Pause();
+        EVENT_CALLBACK MusicEndCallbackDelegate = new EVENT_CALLBACK(FMODCallback);
+    }
+
+    private void OnTriggerEnter(Collider collide)
+    {
+        if (collide.tag == "Player")
+        {
+            IsCollide = true;
+        }
+    }
+
+
+    void Update()
+    {
+        IsMusicPlaying = eventMusic.isValid();
+
+        if (Input.GetKeyUp(KeyCode.E) && IsCollide == true && IsMusicPlaying == false) 
+        {
+            playMusic();
+        }
+    }
+
+    void playMusic()
+    {
+        eventMusic = AudioManager.Instance.PlaySound(AllMyTearsMusic);
+        MusicNote.Play();
+        IsMusicPlaying = true;
+        eventMusic.setCallback(FMODCallback, EVENT_CALLBACK_TYPE.STOPPED);
+    }
+
+    private FMOD.RESULT FMODCallback(EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
+    {
+        // Cast the instance pointer to EventInstance
+        EventInstance instance = new EventInstance(instancePtr);
+
+        if (type == EVENT_CALLBACK_TYPE.STOPPED)
+        {
+            Debug.Log("Sound finished playing!");
+            MusicNote.Stop();
+            IsMusicPlaying = false;
+        }
+        instance.release();
+        return FMOD.RESULT.OK;
+    }
+
+
+}
