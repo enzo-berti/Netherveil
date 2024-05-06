13f2449179b6581403a3c1b2db39bcd3f049f96e
naoki18 <jadras.dunkan@gmail.com>
2024-05-06 14:22:22 +0200

    reflection fireball

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Interface/IReflectable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReflectable
+{
+    Vector3 Direction { get; set; }
+    public bool IsReflected { get; set; }
+    public void Reflect()
+    {
+        Direction = -Direction;
+        IsReflected = true;
+    }
+}
