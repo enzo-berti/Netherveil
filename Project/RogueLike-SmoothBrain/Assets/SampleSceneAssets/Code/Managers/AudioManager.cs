31902693a9b4d24fae1a57a8b04c8828afdb537c
Léo <leo413568@gmail.com>
2024-05-07 14:21:59 +0200

    dash shield sounds

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/AudioManager.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Managers/AudioManager.cs
@@ -43,6 +43,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] EventReference itemBuySFX;
     [SerializeField] EventReference damnationVeilSFX;
     [SerializeField] EventReference divineShieldSFX;
+    [SerializeField] EventReference dashShieldSFX;
 
     public EventReference QuestObtainedSFX { get => questObtainedSFX; }
     public EventReference QuestFinishedSFX { get => questFinishedSFX; }
@@ -63,6 +64,7 @@ public class AudioManager : MonoBehaviour
     public EventReference ItemBuySFX { get => itemBuySFX; }
     public EventReference DamnationVeilSFX { get => damnationVeilSFX; }
     public EventReference DivineShieldSFX { get => divineShieldSFX; }
+    public EventReference DashShieldSFX { get => dashShieldSFX; }
 
     private static AudioManager instance = null;
     public static AudioManager Instance
