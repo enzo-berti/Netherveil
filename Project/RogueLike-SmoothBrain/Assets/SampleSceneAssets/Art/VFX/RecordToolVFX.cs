8f8074169300926d84850434f975cda93411a887
Nicromell <71634044+Nicromell@users.noreply.github.com>
2024-05-09 22:01:23 +0200

    VFX Recorder Script

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Art/VFX/RecordToolVFX.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class RenderVFX : MonoBehaviour
+{
+    VisualEffect[] VFXs;
+    [SerializeField] float WaitTime;
+    void Start()
+    {
+        var VFXFounded = FindObjectsOfType<VisualEffect>();
+        VFXs = VFXFounded;
+        foreach (VisualEffect effect in VFXs)
+        {
+            Debug.Log(effect.name);
+        }
+    }
+
+    IEnumerator PlayAllVFX()
+    {
+        for (int i = 0; i < VFXs.Length; i++)
+        {
+            while (VFXs[i].aliveParticleCount <= 0)
+            {
+                VFXs[i].Play();
+                yield return new WaitForSeconds(3);
+                VFXs[i].Stop();
+                yield return new WaitForSeconds(2);
+                i++;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PlayAllVFX());
+        }
+    }
+}
