187619edb1aea172821ee31685a7c3439bff6792
Dorian <thesothief@gmail.com>
2024-05-13 15:17:23 +0200

    Cinematic

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/MoveTowardUpdate.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/MoveTowardUpdate.cs
@@ -1,16 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class dlamerd : MonoBehaviour
+public class MoveTowardUpdate : MonoBehaviour
 {
-    [SerializeField] float speed;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed;
 
-    // Update is called once per frame
     void Update()
     {
         gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, gameObject.transform.position + transform.forward, Time.deltaTime * speed);
