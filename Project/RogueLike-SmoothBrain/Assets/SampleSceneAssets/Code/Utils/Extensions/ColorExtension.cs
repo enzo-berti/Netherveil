f1c9a8d4059cc7d91444f23cc8aeeb79b25ebef3
naoki18 <jadras.dunkan@gmail.com>
2024-05-13 14:43:03 +0200

    ColorExtension pour pouvoir faire des putains de constructeur de couleur avec du hexadecimal parce qu'apparemment c'est une tache trop complexe pour unity qui n'est pas capable d'avoir le constructeur le plus évident de la planète dans leur class couleur. Enfin je leur en veux pas trop juste un peu quoi parce que c'est pas super dur à faire, mais bon

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Utils/Extensions/ColorExtension.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public static class ColorExtension
+{
+    public static Color Color(string hex)
+    {
+        string redStr = (hex.Substring(0, 2));
+        string greenStr = (hex.Substring(2, 2));
+        string blueStr = (hex.Substring(4, 2));
+
+        float red = redStr.FromHexString() / 255f;
+        float green = greenStr.FromHexString() / 255f;
+        float blue = blueStr.FromHexString() / 255f;
+        UnityEngine.Debug.Log(red + " " + green + " " + blue);
+        return new Color(red, green, blue);
+    }
+
+    private static int FromHexString(this string hexString)
+    {
+        int num = Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+
+        return num;
+    }
+}
