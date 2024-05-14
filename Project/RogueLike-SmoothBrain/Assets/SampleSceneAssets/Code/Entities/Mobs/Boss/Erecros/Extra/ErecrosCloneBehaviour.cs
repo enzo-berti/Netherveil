48a00080a89cff9d0446e5c889c377486480077c
Dorian <thesothief@gmail.com>
2024-05-14 18:53:20 +0200

    Editor

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Boss/Erecros/Extra/ErecrosCloneBehaviour.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Boss/Erecros/Extra/ErecrosCloneBehaviour.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
-using UnityEngine.WSA;
 
 public class ErecrosCloneBehaviour : MonoBehaviour
 {
