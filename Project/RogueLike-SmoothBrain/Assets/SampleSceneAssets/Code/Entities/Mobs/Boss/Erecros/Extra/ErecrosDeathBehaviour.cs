528d994b0b00cfa485ef7fe63b25bf739c6bd32a
Dorian <thesothief@gmail.com>
2024-05-19 14:13:40 +0200

    Erecros

---
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Entities/Mobs/Boss/Erecros/Extra/ErecrosDeathBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ErecrosDeathBehaviour : StateMachineBehaviour
+{
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Destroy(Instantiate(GameResources.Get<GameObject>("VFX_Death"), animator.transform.parent.position, Quaternion.identity), 3f);
+        Destroy(animator.transform.parent.parent.gameObject);
+
+        LevelLoader.current.LoadScene("Outro", true);
+    }
+}
