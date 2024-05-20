4074e4910254e50208d1227841c47c62de83c845
Léo <leo413568@gmail.com>
2024-05-20 14:48:45 +0200

    Update ItemFrame.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/HUD/ItemFrame.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/UI/HUD/ItemFrame.cs
@@ -1,23 +1,60 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class ItemFrame : MonoBehaviour
+public class ItemFrame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] protected Image background;
     [SerializeField] protected Image item;
+    private Button itemButton;
+    private Item itemInstance; // Reference to the Item instance
 
-    public void SetFrame(Sprite background, Sprite item)
+    public UnityEvent onPointerEnter;
+    public UnityEvent onPointerExit;
+
+    private void Awake()
+    {
+        if (item != null && item.GetComponent<Button>() == null)
+        {
+            itemButton = item.gameObject.AddComponent<Button>();
+        }
+        else
+        {
+            itemButton = item.GetComponent<Button>();
+        }
+    }
+
+    public void SetFrame(Sprite backgroundSprite, Sprite itemSprite)
     {
         this.background.gameObject.SetActive(true);
         this.item.gameObject.SetActive(true);
 
-        this.background.sprite = background;
-        this.item.sprite = item;
+        this.background.sprite = backgroundSprite;
+        this.item.sprite = itemSprite;
 
-        if (background == null)
+        if (backgroundSprite == null)
             this.background.gameObject.SetActive(false);
-        if (item == null)
+        if (itemSprite == null)
             this.item.gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onPointerEnter?.Invoke();
+        if (itemInstance != null)
+        {
+            //Debug.Log(itemInstance.GetComponent<ItemDescription>().TogglePanel(true));
+            Debug.Log("show Description");
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onPointerExit?.Invoke();
+        if (itemInstance != null)
+        {
+            Debug.Log("Hide description");
+        }
     }
 }
