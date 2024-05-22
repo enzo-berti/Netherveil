0f2880e5c4ba38c45392e63395cbc290db624eed
Léo <leo413568@gmail.com>
2024-05-22 21:22:40 +0200

    Update BezierUtility.cs

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Tools/BezierUtility.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Tools/BezierUtility.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 
 public static class BezierUtility
 {
-    public static Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+
+    public static Vector3 CalculateQuadraticBezierPoint(float _t, Vector3 _start, Vector3 _firtPointOfControl, Vector3 _end)
     {
-        float u = 1 - t;
-        float tt = t * t;
+        float u = 1 - _t;
+        float tt = _t * _t;
         float uu = u * u;
-        float ttt = tt * t;
+
+        Vector3 p = uu * _start;
+        p += 2 * u * _t * _firtPointOfControl;
+        p += tt * _end;
+
+        return p;
+    }
+
+    public static Vector3 CalculateCubicBezierPoint(float _t, Vector3 _start, Vector3 _firtPointOfControl, Vector3 _secondePointOfControl, Vector3 _end)
+    {
+        float u = 1 - _t;
+        float tt = _t * _t;
+        float uu = u * u;
+        float ttt = tt * _t;
         float uuu = uu * u;
 
-        Vector3 p = uuu * p0; 
-        p += 3 * uu * t * p1; 
-        p += 3 * u * tt * p2; 
-        p += ttt * p3;
+        Vector3 p = uuu * _start; 
+        p += 3 * uu * _t * _firtPointOfControl; 
+        p += 3 * u * tt * _secondePointOfControl; 
+        p += ttt * _end;
 
         return p;
     }
