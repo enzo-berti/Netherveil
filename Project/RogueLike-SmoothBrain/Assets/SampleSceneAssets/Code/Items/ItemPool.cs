964851a43a41d01cf4ae78a1083218fe927d84ab
naoki18 <jadras.dunkan@gmail.com>
2024-05-22 11:34:57 +0200

    using

--- Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/ItemPool.cs
+++ Project/RogueLike-SmoothBrain/Assets/SampleSceneAssets/Code/Items/ItemPool.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
 using Map.Generation;
-using static UnityEditor.Progress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ItemPool
 {
